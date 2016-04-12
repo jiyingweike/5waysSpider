347ee38a8dfb14dc5d37e7d7ef7df82a495d82b9
horsttop <horsttop@163.com>
2016-04-12 11:05:01 +0800

    update python files

---
+++ python/img_python.py
@@ -0,0 +1,25 @@
+#coding=utf-8
+import urllib
+import re
+
+def getHtml(url):
+    page = urllib.urlopen(url)
+    html = page.read()
+    return html
+
+def getImg(html):
+    reg = r'(http:[^\s]*?.jpg)'
+    #reg = r'src="(.+?\.jpg)" pic_ext'
+    imgre = re.compile(reg)
+    imglist = re.findall(imgre,html)
+    x = 0
+    for imgurl in imglist:
+        #urllib.urlretrieve(imgurl,'%s.jpg' % x)
+        print(imgurl)
+        x+=1
+#提取10页图片
+for i in range(1930,1940):
+    html=getHtml("http://jandan.net/ooxx/page-"+str(i)+"#comments")
+    print getImg(html)
+
+getImg(html)
